5d1d66bb4d7ffe6f25ccc283da1d43b93e478465
Anthony Pogliano <poglianoster@gmail.com>
2025-05-29 15:26:42 -0600

    Init monorepo

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Mood Journal</h1>
+      <div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
