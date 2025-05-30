93c6759eb500e31452ca9501b624be1384503dae
Anthony Pogliano <poglianoster@gmail.com>
2025-05-29 23:46:37 -0600

    Successful query from client to server, returned data from DB to client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,25 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './utils/trpc'
+import { useQuery } from '@tanstack/react-query'
+import { trpc } from './utils/trpc'
+
+function UserList() {
+  const getUsers = useQuery(trpc.user.getAllUsers.queryOptions())
+  console.log('Get users result: ', getUsers?.data)
+  return <div>User List</div>
+}
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Mood Journal</h1>
+      <UserList />
       <div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-    </>
+    </QueryClientProvider>
   )
 }
 
