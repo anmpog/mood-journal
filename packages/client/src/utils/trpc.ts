c2b2aa0bb970cd4567a97d696a149cadcee8aded
Anthony Pogliano <poglianoster@gmail.com>
2025-06-01 18:14:49 -0600

    changing import to be from dependency instead of relying on relative path

--- packages/client/src/utils/trpc.ts
+++ packages/client/src/utils/trpc.ts
@@ -1,7 +1,7 @@
 import { QueryClient } from '@tanstack/react-query'
 import { createTRPCClient, httpBatchLink } from '@trpc/client'
 import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query'
-import type { AppRouter } from '../../../server/src/router'
+import type { AppRouter } from 'trpc-server/src/router'
 export const queryClient = new QueryClient()
 const trpcClient = createTRPCClient<AppRouter>({
   links: [httpBatchLink({ url: 'http://localhost:4000/trpc' })],
