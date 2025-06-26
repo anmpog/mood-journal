dede606d9035c2558a89ecc5ddc96c5913085931
Anthony Pogliano <poglianoster@gmail.com>
2025-06-25 22:44:08 -0600

    Yarn version

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,14 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './utils/trpc'
-import UserList from './components/Users.tsx'
-import CreateUser from './components/CreateUser.tsx'
+import UserTable from './components/User/UserTable/UserTable.tsx'
+import CreateUser from './components/User/CreateUser.tsx'
 
 function App() {
   return (
     <div className='min-w-xs max-w-5xl mx-auto p-4'>
       <QueryClientProvider client={queryClient}>
         <h1>Mood Journal</h1>
-        <UserList />
+        <UserTable />
         <CreateUser />
       </QueryClientProvider>
     </div>
