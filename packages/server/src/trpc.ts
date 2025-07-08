4758f0eff35cc27dd3ed8443d44bbbe6be583cff
Anthony Pogliano <poglianoster@gmail.com>
2025-07-07 19:28:11 -0600

    Adding inner context, reorganizing TRPC init/config, stripping unused stuff in Prisma file

--- packages/server/src/trpc.ts
+++ packages/server/src/trpc.ts
@@ -1,11 +1,33 @@
 import { initTRPC } from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
+import { prisma } from './prisma'
 
-type Context = Awaited<ReturnType<typeof createContext>>
+// Expand this to add session
+interface CreateInnerContextOptions
+  extends Partial<trpcExpress.CreateExpressContextOptions> {
+  session?: {}
+}
 
-export const createContext = ({
-  req,
-  res,
-}: trpcExpress.CreateExpressContextOptions) => ({})
+export async function createContextInner(opts?: CreateInnerContextOptions) {
+  return {
+    prisma,
+    session: opts?.session,
+  }
+}
+
+export async function createContext(
+  opts: trpcExpress.CreateExpressContextOptions
+) {
+  const contextInner = await createContextInner()
+
+  return {
+    ...contextInner,
+    req: opts.req,
+    res: opts.res,
+  }
+}
+
+// Docs say to infer type of inner context since it's always available
+type Context = Awaited<ReturnType<typeof createContextInner>>
 
 export const trpc = initTRPC.context<Context>().create()
