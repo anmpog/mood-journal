d66ba44e7e91d23062751c633876a96216579e3b
Anthony Pogliano <poglianoster@gmail.com>
2025-10-21 18:09:02 -0600

    chore: adding a shared package for types/enums/etc

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,8 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,7 +16,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
+      '@shared': path.resolve(__dirname, '../shared/src'),
     },
   },
 })
