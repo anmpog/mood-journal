d005f636f55a6898f5b4b39b857cb33db876ad7c
Anthony Pogliano <poglianoster@gmail.com>
2025-12-14 00:36:34 -0700

    feature: CreateActivityEntry dialog UI
    CreateActivityEntry form component has basic dialog implementation. It
    renders activities and contains its own state. I want to figure out how
    to let its Formik-controlled state be available to a parent form (also
    controlled by Formik). This represents a step in the evolution of this
    component, not a complete feature/UI implmentation.

--- packages/client/src/components/CreateActivityEntry.tsx
+++ packages/client/src/components/CreateActivityEntry.tsx
@@ -1,12 +1,10 @@
 import { activityLogTitleEnum } from '@/enums/activityLogTitleEnum'
-import { durationScaleUISchema } from '@/schemas/durationScaleSchema'
 import { intensityScaleUISchema } from '@/schemas/intensityScaleSchema'
 import { qualitativeScaleUISchema } from '@/schemas/qualitativeScaleSchema'
 import { quantitativeScaleUISchema } from '@/schemas/quantitativeScaleSchema'
-import { Field, FieldProps, Form, Formik } from 'formik'
+import { Field, FieldProps, Formik } from 'formik'
 import startCase from 'lodash.startcase'
 import { useState } from 'react'
-import { z } from 'zod'
 import { Button } from './ui/button'
 import { Label } from './ui/label'
 import {
@@ -17,10 +15,18 @@ import {
   SelectValue,
 } from './ui/select'
 
-import { X } from 'lucide-react'
-import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
-
-type ActivityTitleType = z.infer<typeof activityLogTitleEnum>
+import { activityDurationScaleUISchema } from '@/schemas/activityDurationScaleSchema'
+import { ActivityEntryDefaultValuesType } from '@/types/ActivityEntryDefaultValues'
+import { ClientActivityEntryType } from '@/types/ClientActivityEntry'
+import { ActivityEntry } from './ActivityEntry'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog'
 
 const activityTitleOptions = activityLogTitleEnum.options.map((option) => {
   return {
@@ -29,19 +35,7 @@ const activityTitleOptions = activityLogTitleEnum.options.map((option) => {
   }
 })
 
-interface ClientDefaultValuesType {
-  activityTitle: ActivityTitleType | ''
-  durationRating: string
-  intensityRating: string
-  qualitativeRating: string
-  quantitativeRating: string
-}
-
-interface ClientActivityEntryType extends ClientDefaultValuesType {
-  activityId: string
-}
-
-const defaultFormValues: ClientDefaultValuesType = {
+const defaultFormValues: ActivityEntryDefaultValuesType = {
   activityTitle: '',
   durationRating: '',
   intensityRating: '',
@@ -49,48 +43,12 @@ const defaultFormValues: ClientDefaultValuesType = {
   quantitativeRating: '',
 }
 
-const ActivityEntry = ({
-  activity: {
-    activityTitle,
-    durationRating,
-    intensityRating,
-    qualitativeRating,
-    quantitativeRating,
-    activityId,
-  },
-  handleRemoveActivity,
-}: {
-  activity: ClientActivityEntryType
-  handleRemoveActivity: (activityId: string) => void
-}) => {
-  return (
-    <Card className='basis-1/3 relative'>
-      <Button
-        variant='ghost'
-        size='icon-sm'
-        className='rounded-full absolute top-2 right-2'
-        onClick={() => handleRemoveActivity(activityId)}
-      >
-        <X />
-      </Button>
-      <CardHeader>
-        <CardTitle>Activity Title: {activityTitle}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p>Duration: {durationRating}</p>
-        <p>Intensity: {intensityRating}</p>
-        <p>Quality: {qualitativeRating}</p>
-        <p>Quantity: {quantitativeRating}</p>
-      </CardContent>
-    </Card>
-  )
-}
-
 // Child form for creating journal entry, but I want it to maintain its own state
 export const CreateActivityEntry = () => {
   const [activities, setActivities] = useState<ClientActivityEntryType[]>([])
+  const [activityDialogOpen, setActivityDialogOpen] = useState<boolean>(false)
 
-  const handleAddActivity = (activity: ClientDefaultValuesType) => {
+  const handleAddActivity = (activity: ActivityEntryDefaultValuesType) => {
     const activityId = crypto.randomUUID()
     setActivities([
       ...activities,
@@ -103,15 +61,23 @@ export const CreateActivityEntry = () => {
 
   const handleRemoveActivity = (
     activityId: ClientActivityEntryType['activityId']
-  ) => {
+  ): void => {
     setActivities(activities.filter((a) => a.activityId !== activityId))
+  }
+
+  const handleResetActivities = (): void => {
+    setActivities([])
+  }
+
+  const handleCloseActivityDialog = (): void => {
+    setActivityDialogOpen(false)
   }
 
   return (
     <>
-      <h3>Activity Entry UI</h3>
+      <h3>Activity Entries</h3>
       {activities.length > 0 ? (
-        <div className='flex flex-wrap gap-2'>
+        <div className='grid grid-cols-3 gap-2'>
           {activities.map((activity) => (
             <ActivityEntry
               activity={activity}
@@ -121,13 +87,15 @@ export const CreateActivityEntry = () => {
           ))}
         </div>
       ) : (
-        <p>There are no activity entries</p>
+        <p>There are no activity entries yet.</p>
       )}
       <Formik
         initialValues={defaultFormValues}
         onSubmit={(values, { resetForm }) => {
+          console.log('Form values: ', values)
           handleAddActivity(values)
           resetForm()
+          handleCloseActivityDialog()
         }}
       >
         {(formik) => {
@@ -135,53 +103,77 @@ export const CreateActivityEntry = () => {
           // can submit if there is a valid title AND formik dirty
           const canSubmit = validTitle && formik.dirty
           return (
-            <Form>
-              <Field name='activityTitle' id='activityTitle'>
-                {({ field, form }: FieldProps) => {
-                  return (
-                    <>
-                      <Label htmlFor={field.name}>Activity Title</Label>
-                      <Select
-                        value={field.value}
-                        onValueChange={(value: string) => {
-                          form.setFieldValue(field.name, value)
-                        }}
-                      >
-                        <SelectTrigger>
-                          <SelectValue placeholder='Select an Activity' />
-                        </SelectTrigger>
-                        <SelectContent>
-                          {activityTitleOptions.map((opt, index) => {
-                            return (
-                              <SelectItem value={opt.value} key={index}>
-                                {opt.label}
-                              </SelectItem>
-                            )
-                          })}
-                        </SelectContent>
-                      </Select>
-                    </>
-                  )
-                }}
-              </Field>
-              <Field name='durationRating' id='durationRating'>
-                {({ field, form }: FieldProps) => {
-                  return (
-                    <>
-                      <Label htmlFor={field.name}>Duration Rating</Label>
-                      <Select
-                        value={field.value}
-                        onValueChange={(value) => {
-                          form.setFieldValue(field.name, value)
-                        }}
-                      >
-                        <SelectTrigger>
-                          <SelectValue placeholder='Select a Duration' />
-                        </SelectTrigger>
-                        <SelectContent>
-                          {durationScaleUISchema.map(
-                            ({ value, label }, index) => {
-                              return (
+            <Dialog
+              open={activityDialogOpen}
+              onOpenChange={setActivityDialogOpen}
+            >
+              <div className='flex gap-2 mt-5'>
+                <DialogTrigger asChild>
+                  <Button variant='outline'>Add Activity</Button>
+                </DialogTrigger>
+                <Button
+                  variant='destructive'
+                  disabled={!activities.length}
+                  onClick={handleResetActivities}
+                >
+                  Clear Activities
+                </Button>
+              </div>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Add an Activity</DialogTitle>
+                  <DialogDescription>
+                    Add an activity to your journal entry to track its effect on
+                    your mood over time. Select the options that best describe
+                    the activity you want to track.
+                  </DialogDescription>
+                </DialogHeader>
+                {/* <Form> */}
+                <Field name='activityTitle' id='activityTitle'>
+                  {({ field, form }: FieldProps) => {
+                    return (
+                      <>
+                        <Label htmlFor={field.name}>Activity Title</Label>
+                        <Select
+                          value={field.value}
+                          onValueChange={(value: string) => {
+                            form.setFieldValue(field.name, value)
+                          }}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder='Select an Activity' />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {activityTitleOptions.map(
+                              ({ value, label }, index) => (
+                                <SelectItem value={value} key={index}>
+                                  {label}
+                                </SelectItem>
+                              )
+                            )}
+                          </SelectContent>
+                        </Select>
+                      </>
+                    )
+                  }}
+                </Field>
+                <Field name='durationRating' id='durationRating'>
+                  {({ field, form }: FieldProps) => {
+                    return (
+                      <>
+                        <Label htmlFor={field.name}>Duration Rating</Label>
+                        <Select
+                          value={field.value}
+                          onValueChange={(value) => {
+                            form.setFieldValue(field.name, value)
+                          }}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder='Select a Duration' />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {activityDurationScaleUISchema.map(
+                              ({ value, label }, index) => (
                                 <SelectItem
                                   value={value?.toString()}
                                   key={index}
@@ -189,18 +181,16 @@ export const CreateActivityEntry = () => {
                                   {label}
                                 </SelectItem>
                               )
-                            }
-                          )}
-                        </SelectContent>
-                      </Select>
-                    </>
-                  )
-                }}
-              </Field>
-              <Field name='intensityRating' id='intensityRating'>
-                {({ field, form }: FieldProps) => {
-                  return (
-                    <>
+                            )}
+                          </SelectContent>
+                        </Select>
+                      </>
+                    )
+                  }}
+                </Field>
+                <Field name='intensityRating' id='intensityRating'>
+                  {({ field, form }: FieldProps) => {
+                    ;<>
                       <Label htmlFor={field.name}>Intensity Rating</Label>
                       <Select
                         value={field.value}
@@ -213,8 +203,34 @@ export const CreateActivityEntry = () => {
                         </SelectTrigger>
                         <SelectContent>
                           {intensityScaleUISchema.map(
-                            ({ value, label }, index) => {
-                              return (
+                            ({ value, label }, index) => (
+                              <SelectItem value={value?.toString()} key={index}>
+                                {label}
+                              </SelectItem>
+                            )
+                          )}
+                        </SelectContent>
+                      </Select>
+                    </>
+                  }}
+                </Field>
+                <Field name='qualitativeRating' id='qualitativeRating'>
+                  {({ field, form }: FieldProps) => {
+                    return (
+                      <>
+                        <Label htmlFor={field.name}>Qualitative Rating</Label>
+                        <Select
+                          value={field.value}
+                          onValueChange={(value) => {
+                            form.setFieldValue(field.name, value)
+                          }}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder='Select a Quality' />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {qualitativeScaleUISchema.map(
+                              ({ value, label }, index) => (
                                 <SelectItem
                                   value={value?.toString()}
                                   key={index}
@@ -222,32 +238,30 @@ export const CreateActivityEntry = () => {
                                   {label}
                                 </SelectItem>
                               )
-                            }
-                          )}
-                        </SelectContent>
-                      </Select>
-                    </>
-                  )
-                }}
-              </Field>
-              <Field name='qualitativeRating' id='qualitativeRating'>
-                {({ field, form }: FieldProps) => {
-                  return (
-                    <>
-                      <Label htmlFor={field.name}>Qualitative Rating</Label>
-                      <Select
-                        value={field.value}
-                        onValueChange={(value) => {
-                          form.setFieldValue(field.name, value)
-                        }}
-                      >
-                        <SelectTrigger>
-                          <SelectValue placeholder='Select a Quality' />
-                        </SelectTrigger>
-                        <SelectContent>
-                          {qualitativeScaleUISchema.map(
-                            ({ value, label }, index) => {
-                              return (
+                            )}
+                          </SelectContent>
+                        </Select>
+                      </>
+                    )
+                  }}
+                </Field>
+                <Field name='quantitativeRating' id='quantitativeRating'>
+                  {({ field, form }: FieldProps) => {
+                    return (
+                      <>
+                        <Label htmlFor={field.name}>Quantity Rating</Label>
+                        <Select
+                          value={field.value}
+                          onValueChange={(value) => {
+                            form.setFieldValue(field.name, value)
+                          }}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder='Select a Quantity' />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {quantitativeScaleUISchema.map(
+                              ({ value, label }, index) => (
                                 <SelectItem
                                   value={value?.toString()}
                                   key={index}
@@ -255,52 +269,28 @@ export const CreateActivityEntry = () => {
                                   {label}
                                 </SelectItem>
                               )
-                            }
-                          )}
-                        </SelectContent>
-                      </Select>
-                    </>
-                  )
-                }}
-              </Field>
-              <Field name='quantitativeRating' id='quantitativeRating'>
-                {({ field, form }: FieldProps) => {
-                  return (
-                    <>
-                      <Label htmlFor={field.name}>Quantity Rating</Label>
-                      <Select
-                        value={field.value}
-                        onValueChange={(value) => {
-                          form.setFieldValue(field.name, value)
-                        }}
-                      >
-                        <SelectTrigger>
-                          <SelectValue placeholder='Select a Quantity' />
-                        </SelectTrigger>
-                        <SelectContent>
-                          {quantitativeScaleUISchema.map(
-                            ({ value, label }, index) => {
-                              return (
-                                <SelectItem
-                                  value={value?.toString()}
-                                  key={index}
-                                >
-                                  {label}
-                                </SelectItem>
-                              )
-                            }
-                          )}
-                        </SelectContent>
-                      </Select>
-                    </>
-                  )
-                }}
-              </Field>
-              <Button type='submit' disabled={!canSubmit}>
-                Add Activity
-              </Button>
-              <Button type='reset'>Reset Form</Button>
-            </Form>
+                            )}
+                          </SelectContent>
+                        </Select>
+                      </>
+                    )
+                  }}
+                </Field>
+                <div className='flex gap-2 mt-5 justify-end'>
+                  <Button
+                    type='button'
+                    onClick={() => formik.handleSubmit()}
+                    disabled={!canSubmit}
+                  >
+                    Add Activity
+                  </Button>
+                  <Button type='reset' variant='destructive'>
+                    Reset Fields
+                  </Button>
+                </div>
+                {/* </Form> */}
+              </DialogContent>
+            </Dialog>
           )
         }}
       </Formik>
