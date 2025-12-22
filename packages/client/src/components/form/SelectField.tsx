92b4f3d207caa6a6d316cb071ceb66c32a7ed8e1
Anthony Pogliano <poglianoster@gmail.com>
2025-12-22 00:10:09 -0700

    feat: SelectField component to reduce boilerplate
    Create SelectField that combines common UI elements to reduce
    boilerplate.

---
+++ packages/client/src/components/form/SelectField.tsx
@@ -0,0 +1,51 @@
+import { useField } from 'formik'
+import { Label } from '../ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
+
+interface SelectFieldProps {
+  name: string
+  label: string
+  placeholderText: string
+  optionsArr: {
+    value: string | number
+    label: string
+  }[]
+}
+
+const SelectField = ({
+  name,
+  label,
+  placeholderText,
+  optionsArr,
+}: SelectFieldProps) => {
+  const [field, _, helpers] = useField(name)
+  return (
+    <>
+      <Label htmlFor={name}>{label}</Label>
+      <Select
+        name={name}
+        value={field.value}
+        onValueChange={(value) => helpers.setValue(value)}
+      >
+        <SelectTrigger>
+          <SelectValue placeholder={placeholderText} />
+        </SelectTrigger>
+        <SelectContent>
+          {optionsArr.map(({ value, label }, index) => (
+            <SelectItem value={value.toString()} key={index}>
+              {label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </>
+  )
+}
+
+export default SelectField
