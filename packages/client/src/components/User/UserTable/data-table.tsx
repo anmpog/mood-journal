7c815369b13840e07fa5fcad64d66620fe6689c9
Anthony Pogliano <poglianoster@gmail.com>
2026-01-14 16:01:15 -0700

    chore(format): updating prettierignore
    Updated prettierignore to not format components added by shadcn, ran
    formatting on additional files.

--- packages/client/src/components/User/UserTable/data-table.tsx
+++ packages/client/src/components/User/UserTable/data-table.tsx
@@ -47,7 +47,7 @@ export function DataTable<TData, TValue>({
                       ? null
                       : flexRender(
                           header.column.columnDef.header,
-                          header.getContext()
+                          header.getContext(),
                         )}
                   </TableHead>
                 )
@@ -78,7 +78,7 @@ export function DataTable<TData, TValue>({
                       >
                         {flexRender(
                           cell.column.columnDef.cell,
-                          cell.getContext()
+                          cell.getContext(),
                         )}
                       </TableCell>
                     )
